83c6da9ac58663e611a40883aaf18efa56ad2373
Maicon Castagna <riotpgames@gmail.com>
2021-06-23 13:02:32 -0300

    Config Espaço Maker && Edit Maker

--- routes/maker.route.js
+++ routes/maker.route.js
@@ -11,6 +11,16 @@ const md_redirect = require('../middlewares/md_redirect');
 //----------------------------------------------------------------------
 // Rotas personalizadas
 
+router.get('/maker_edit', async (req, res) => {
+    const idMaker = req.session.makerId;
+    const maker = await Maker.findOne({
+        where: {
+            id: idMaker
+        }
+    })
+    res.render('maker_edit', { maker });
+});
+
 router.get('/mkhome', async (req, res) => {
     const makers = await Maker.findAll();
     res.render('home_makers', { makers });
@@ -23,7 +33,16 @@ router.get('/maker/:Id', async (req, res) => {
             id: makerId
         }
     });
-    console.log(req.session.loggedIn);
+    return res.render('maker', { maker });
+});
+
+router.get('/esp_maker', async (req, res) => {
+    const makerId = req.session.makerId;
+    const maker = await Maker.findOne({
+        where: {
+            id: makerId
+        }
+    });
     return res.render('maker', { maker });
 });
 
@@ -31,7 +50,6 @@ router.get('/maker_signup', async (req, res) => {
     const makers = await Maker.findAll();
     res.render('maker_signup', { makers })
 });
-
 router.get('/maker_signin', async (req, res) => {
     const makers = await Maker.findAll();
     res.render('maker_signin', { makers });
@@ -45,7 +63,6 @@ router.get('/:id/vis', md_redirect, async (req, res) => {
         }
     });
     res.render('maker_visualizar', { maker })
-    console.log(req.session.loggedIn);
 });
 
 //-----------------------------------------------------------
@@ -80,7 +97,7 @@ router.get('/:Id/edit', async (req, res) => {
     return res.render('maker_edit', { maker });
 });
 
-router.post('/:id', async (req, res) => {
+router.post('/:id/edit', async (req, res) => {
     const id = req.params.id;
 
     const maker = await Maker.findByPk(id);
@@ -91,7 +108,7 @@ router.post('/:id', async (req, res) => {
     _.assign(maker, req.body);
 
     await maker.save();
-    res.redirect('/makers');
+    res.redirect('/maker/' + id);
 });
 
 router.delete('/:id', async (req, res) => {
