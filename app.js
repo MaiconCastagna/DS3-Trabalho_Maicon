674ca0f7859d9a50d8a01f4a12006452e332ac08
Maicon Castagna <riotpgames@gmail.com>
2021-06-23 15:21:01 -0300

    Commit pré Project "association"

--- app.js
+++ app.js
@@ -6,15 +6,20 @@ const session = require('express-session');
 const hbs = require('hbs');
 
 //----------------------------------------------------------
+// Resgata arquivo de rota
 
 const AuthRoute = require('./routes/auth.route');
 const IndexRoute = require('./routes/index.route');
 const MakerRoutes = require('./routes/maker.route');
+const ProjectRoutes = require('./routes/project.route');
 
 //----------------------------------------------------------
 
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
+
+//----------------------------------------------------------
+// Express Session
 
 // Configura express-session
 var expiraCookie = new Date(Date.now() + 60 * 60 * 1000);
@@ -26,7 +31,6 @@ app.use(session({
     expires: expiraCookie
   }
 }));
-
 // Adiciona sessão em toda resposta do servidor
 app.use(function (req, res, next) {
   res.locals.session = req.session;
@@ -34,7 +38,7 @@ app.use(function (req, res, next) {
 });
 
 //----------------------------------------------------------
-//Registra os Helpers
+//Registra Helpers //TO_DO MUDAR DE LUGAR
 
 hbs.registerHelper('calcula-data', (date) => {
   date = new Date(date);
@@ -50,6 +54,7 @@ hbs.registerHelper('calcula-data', (date) => {
 app.use('/', IndexRoute);
 app.use('/auth', AuthRoute);
 app.use('/makers', MakerRoutes);
+app.use('/projects', ProjectRoutes);
 
 //----------------------------------------------------------
 // Configuração da Template Engine
---
+++ models/project.js
@@ -0,0 +1,28 @@
+function Project(sequelize, DataType) {
+    const attributes = {
+
+        name: {
+            type: DataType.STRING,
+            allowNull: false
+        },
+        image: {
+            type: DataType.STRING,
+            allowNull: true
+        },
+        description: {
+            type: DataType.STRING,
+            allowNull: false
+        },
+        authorId: {
+            type: DataType.INTEGER,
+            allowNull: false
+        },
+
+    };
+
+    const options = {};
+
+    return sequelize.define('project', attributes, options);
+}
+
+module.exports = Project;
